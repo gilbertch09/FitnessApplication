a03572c5c22c67dd5b54d64fb0b6eb79f2a78183
Gilbert <Gilbert-Christoper>
2020-06-21 22:20:10 +0700

    Create button to make sound and vibrate

--- app/src/main/java/com/example/fitnessapplication/WorkoutAct.java
+++ app/src/main/java/com/example/fitnessapplication/WorkoutAct.java
@@ -3,8 +3,13 @@ package com.example.fitnessapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -33,12 +38,26 @@ public class WorkoutAct extends AppCompatActivity {
         bgprogress = (View) findViewById(R.id.bgprogress);
 
         // give an event to another page
+        final MediaPlayer mp = MediaPlayer.create(this,R.raw.countdown);
         btnexercise.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent a = new Intent(WorkoutAct.this, StartWorkAct.class);
                 a.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                 startActivity(a);
+                mp.start();
+                vibrateNow(500);
+            }
+
+            private void vibrateNow(long millis) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    ((Vibrator) getSystemService(VIBRATOR_SERVICE))
+                            .vibrate(VibrationEffect.createOneShot(millis, VibrationEffect.DEFAULT_AMPLITUDE));
+                }
+
+                else {
+                    ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(millis);
+                }
             }
         });
 
@@ -61,8 +80,6 @@ public class WorkoutAct extends AppCompatActivity {
 
         fitfourtitle =(TextView) findViewById(R.id.fitfourtitle);
         fitfourdesc =(TextView) findViewById(R.id.fitfourdesc);
-
-
 
     }
 }
